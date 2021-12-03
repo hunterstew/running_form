0b695f4c01593598ad87f62a602293432cd22adb
Hunter <71897156+hunterstew@users.noreply.github.com>
2021-12-02 18:51:08 -0600

    add support for 3 runners

--- sketch.js
+++ sketch.js
@@ -11,12 +11,17 @@ let playVideo = true;
 let times = [];
 let groundContactTime = [];
 let test = [];
+let test2 = [];
 test[0] = [];
 test[1] = [];
+test[2] = [];
+test2[0] = [];
+test2[1] = [];
+test2[2] = [];
 
 // change this variable to get the correct video and metrics for that speed
-let speed = 8; //6,8,12
-let people = 2; //1 for single person; 2-6 for multi-person
+let speed = 8; //6,8,12 -- only for single person
+let people = 3; //1 for single person; 2-6 for multi-person //todo convert to loop for support for 4-6 people
 let obscure = true; // true or false
 
 if (people == 1) {
@@ -44,7 +49,7 @@ if (people == 1) {
     throw new Error('invalid speed');
   }
 }
-else if (people > 1 && people < 7) {
+else if (people == 2) {
   rightAnkley[0] = [];
   rightHipy[0] = [];
   rightAnkley[1] = [];
@@ -67,6 +72,35 @@ else if (people > 1 && people < 7) {
     hip_confidence = .7;
     ankle_confidence = .35;
     video = 'videos/not_obscured_trim.mp4';
+  }
+}
+else if (people > 2 && people < 7) {
+  rightAnkley[0] = [];
+  rightHipy[0] = [];
+  rightAnkley[1] = [];
+  rightHipy[1] = [];
+  rightAnkley[2] = [];
+  rightHipy[2] = [];
+  times[0] = [];
+  times[1] = [];
+  times[2] = [];
+  groundContactTime[0] = [];
+  groundContactTime[1] = [];
+  groundContactTime[2] = [];
+  var enableTracking = true;
+  var keypoint_confidence = .35;
+
+  if (obscure) {
+    vo_factor = .062;
+    hip_confidence = .6;
+    ankle_confidence = .15;
+    video = 'videos/obscured_3.mp4';
+  }
+  else {
+    vo_factor = .07;
+    hip_confidence = .7;
+    ankle_confidence = .15;
+    video = 'videos/not_obscured_3.mp4';
   }
 }
 else {
@@ -176,7 +210,7 @@ function end() {
     console.log('verticalOscillation in in.', vert_osc * vo_factor);
 
   }
-  else {
+  else if (people == 2) {
     end_of_video = true;
     console.log('the end');
     // console.log('test', test)
@@ -285,6 +319,109 @@ function end() {
       console.log('verticalOscillation in in.', vert_osc * vo_factor);
     }
   }
+  else {
+    end_of_video = true;
+    console.log('the end');
+    // console.log('test', test)
+    // console.log('test2', test2)
+    // console.log('rightHip', rightHipy);
+    // console.log('rightAnkle', rightAnkley);
+    // dedup in case the video freezes
+    let dedupAnkle = [];
+    let dedupHip = [];
+    dedupHip[0] = [];
+    dedupHip[1] = [];
+    dedupHip[2] = [];
+    dedupAnkle[0] = [];
+    dedupAnkle[1] = [];
+    dedupAnkle[2] = [];
+    rightAnkley[0].forEach((element, index) => { if (index != 0 && rightAnkley[0][index] == rightAnkley[0][index - 1]) return; dedupAnkle[0].push(rightAnkley[0][index]) })
+    rightAnkley[1].forEach((element, index) => { if (index != 0 && rightAnkley[1][index] == rightAnkley[1][index - 1]) return; dedupAnkle[1].push(rightAnkley[1][index]) })
+    rightAnkley[2].forEach((element, index) => { if (index != 0 && rightAnkley[2][index] == rightAnkley[2][index - 1]) return; dedupAnkle[2].push(rightAnkley[2][index]) })
+    rightHipy[0].forEach((element, index) => { if (index != 0 && rightHipy[0][index] == rightHipy[0][index - 1]) return; dedupHip[0].push(rightHipy[0][index]) })
+    rightHipy[1].forEach((element, index) => { if (index != 0 && rightHipy[1][index] == rightHipy[1][index - 1]) return; dedupHip[1].push(rightHipy[1][index]) })
+    rightHipy[2].forEach((element, index) => { if (index != 0 && rightHipy[2][index] == rightHipy[2][index - 1]) return; dedupHip[2].push(rightHipy[2][index]) })
+
+    // console.log('reduced rightHip', rightHipy);
+    // console.log('reduce rightAnkley', dedupAnkle);
+    let sortedRightAnkley = [];
+    sortedRightAnkley[0] = [];
+    sortedRightAnkley[1] = [];
+    sortedRightAnkley[2] = [];
+
+    for (let j = 0; j < 3; j++) {
+      sortedRightAnkley[j] = [...dedupAnkle[j]];
+      sortedRightAnkley[j].sort();
+      // console.log('rightAnkle', dedupAnkle[j]);
+      // console.log(sortedRightAnkley[j]);
+      hits = sortedRightAnkley[j].length;
+
+      // calculate percentile-based values for steps
+      // 55 and 80 line up best with ground truth values
+      p55 = sortedRightAnkley[j][Math.round(11 * hits / 20)];
+      p80 = sortedRightAnkley[j][Math.round(4 * hits / 5)];
+      // count steps from video
+      i = 0;
+      steps = [];
+      step = false;
+      for (i = 0; i < dedupAnkle[j].length; i++) {
+        // if no recent step detected, see if foot steps
+        if (!step) {
+          if (dedupAnkle[j][i] > p80) {
+            startTime = times[j][i];
+            step = true;
+            steps.push(i);
+            i = i + 4; // skip a few frames to ensure no step gets counted twice
+          }
+        }
+        // if recent step detected, see if foot is high
+        else {
+          if (dedupAnkle[j][i] < p55) {
+            step = false;
+            endTime = times[j][i];
+            groundContactTime[j].push(endTime - startTime);
+          }
+        }
+      }
+      console.log('steps', steps);
+      console.log('steps', steps.length);
+      cadence = steps.length * 2 / (document.getElementsByTagName('video')[0].duration) * 60;
+      console.log('cadence (steps/minute)', cadence);
+      console.log('groundContactTime', groundContactTime[j]);
+      verticalOscillation = [];
+
+      acc = 0;
+      groundContactTime[j].forEach(time => {
+        acc += time;
+      });
+      grct = acc / groundContactTime[j].length;
+      console.log('groundContactTime', grct);
+      samplesPerStep = Math.trunc(dedupHip[j].length / steps.length);
+      for (i = 0; i < steps.length; i++) {
+        maxHipy = minHipy = test = dedupHip[j][samplesPerStep * i];
+        for (k = 0; k < samplesPerStep; k++) {
+          test = dedupHip[j][i * samplesPerStep + k];
+          if (test < minHipy)
+            minHipy = test;
+          else if (test > maxHipy) {
+            maxHipy2 = maxHipy; // remove outlier/false positive
+            maxHipy = test;
+          }
+        }
+
+        verticalOscillation.push(maxHipy2 - minHipy);
+      }
+      console.log('verticalOscillation', verticalOscillation);
+
+      acc2 = 0;
+      verticalOscillation.forEach(osc => {
+        acc2 += osc;
+      });
+      vert_osc = acc2 / verticalOscillation.length;
+      console.log('verticalOscillation in px', vert_osc);
+      console.log('verticalOscillation in in.', vert_osc * vo_factor);
+    }
+  }
 }
 
 
@@ -328,6 +465,7 @@ async function getPoses() {
         }
         // 12 is rightHip
         if (poses[i] && poses[i].keypoints[12].score > hip_confidence) { // use .8 for 8mph; use .7 for 6 and 12 mph
+          test2[i].push([poses[i].keypoints[12].y, poses[i].keypoints[12].score])
           rightHipy[i].push(poses[i].keypoints[12].y);
         }
       }
